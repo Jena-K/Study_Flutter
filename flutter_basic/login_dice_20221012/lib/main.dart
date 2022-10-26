4c49935ed47507f6eab27412c2b5bab91577001d
Jena <lunalik@naver.com>
2022-10-27 03:25:59 +0900

    update

--- flutter_basic/login_dice_20221012/lib/main.dart
+++ flutter_basic/login_dice_20221012/lib/main.dart
@@ -31,65 +31,73 @@ class _LogInState extends State<LogIn> {
           IconButton(icon: Icon(Icons.search), onPressed: () {})
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(padding: EdgeInsets.only(top: 50)),
-            Center(
-              child: Image(
-                image: AssetImage('image/chef.gif'),
-                width: 170,
-                height: 190,
-              ),
-            ),
-            Form(
-              child: Theme(
-                data: ThemeData(
-                  primaryColor: Colors.teal,
-                  inputDecorationTheme: InputDecorationTheme(
-                    labelStyle: TextStyle(
-                      color: Colors.teal,
-                      fontSize: 15,
+      body: Builder(builder: (context) {
+        return GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus(); //focus 가 풀리면 키보드가 사라짐
+          },
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Padding(padding: EdgeInsets.only(top: 50)),
+                Center(
+                  child: Image(
+                    image: AssetImage('image/chef.gif'),
+                    width: 170,
+                    height: 190,
+                  ),
+                ),
+                Form(
+                  child: Theme(
+                    data: ThemeData(
+                      primaryColor: Colors.teal,
+                      inputDecorationTheme: InputDecorationTheme(
+                        labelStyle: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(40),
+                      child: Column(
+                        children: <Widget>[
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Enter "dice"',
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Enter Password',
+                            ),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                          ),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          ButtonTheme(
+                            minWidth: 100,
+                            height: 50,
+                            child: RaisedButton(
+                              color: Colors.orangeAccent,
+                              child: Icon(Icons.arrow_forward,
+                                  color: Colors.white),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-                child: Container(
-                  padding: EdgeInsets.all(40),
-                  child: Column(
-                    children: <Widget>[
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Enter "dice"',
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Enter Password',
-                        ),
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                      ),
-                      SizedBox(
-                        height: 40,
-                      ),
-                      ButtonTheme(
-                        minWidth: 100,
-                        height: 50,
-                        child: RaisedButton(
-                          color: Colors.orangeAccent,
-                          child: Icon(Icons.arrow_forward, color: Colors.white),
-                          onPressed: () {},
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      }),
     );
   }
 }
