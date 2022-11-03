f9f742e244508b7c5502c88f6a48fbbe6bcde8bb
jena <lunalik@naver.com>
2022-11-03 18:02:01 +0900

    start project

--- flutter_basic/weather_app_2021102/lib/screens/loading.dart
+++ flutter_basic/weather_app_2021102/lib/screens/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -7,10 +8,29 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+    fetchData();
+  }
+
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    print(position);
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      print(position);
+    } catch (e) {
+      print('There was a problem with the internet connection.');
+    }
+  }
+
+  void fetchData() async {
+    Response response = await get(Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
+    print(response.body);
   }
 
   @override
@@ -21,9 +41,7 @@ class _LoadingState extends State<Loading> {
           style: ElevatedButton.styleFrom(
             primary: Colors.blue,
           ),
-          onPressed: () {
-            getLocation();
-          },
+          onPressed: null,
           child: Text(
             'Get my location',
             style: TextStyle(color: Colors.white),
